6e29617c8fa1231a61c725e5831ecfa8d9e2f3fb
panruopeng <alhprp@gmail.com>
2017-09-01 18:06:12 +0800

    FIX:增加对于宽度的限制。增加对于传入的html出现标签不配对的情况的处理。

--- pkg/handlers/render.go
+++ pkg/handlers/render.go
@@ -11,6 +11,7 @@ import (
 	"github.com/sempr/goscreenshot/constants"
 	"github.com/sempr/goscreenshot/pkg/lru"
 	"github.com/sempr/goscreenshot/pkg/shot"
+	"log"
 )
 
 // RenderArgs xxx
@@ -53,14 +54,53 @@ func RenderHandler(w http.ResponseWriter, r *http.Request) {
 <head>
 <meta charset="utf8" />
 <style>
-%s
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+        margin: 0;
+        padding: 0;
+        border: 0;
+        font-size: 100%;
+        font: inherit;
+        vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure,
+footer, header, hgroup, menu, nav, section {
+        display: block;
+}
+body {
+        line-height: 1;
+}
+ol, ul {
+        list-style: none;
+}
+blockquote, q {
+        quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+        content: '';
+        content: none;
+}
+table {
+        border-collapse: collapse;
+        border-spacing: 0;
+}
 </style>
+
 </head>
 <body>
-<div id="ACHHcLIkD3">
-%s
-</div>
-<div id="ImgLoadedFlagACHHcLIkD3" style="display:none;">test</div>
 <script>
     document.addEventListener("DOMContentLoaded", function() {
         document.removeEventListener("DOMContentLoaded", arguments.callee, false);
@@ -76,18 +116,27 @@ func RenderHandler(w http.ResponseWriter, r *http.Request) {
             if (complete) {
                 document.getElementById('ImgLoadedFlagACHHcLIkD3').style.display = "block";
             } else {
-                window.setTimeout(f);
+                window.setTimeout(f, 50);
             }
         };
         f();
+        window.setTimeout(function() {
+        	document.getElementById('ImgLoadedFlagACHHcLIkD3').style.display = "block";
+        }, 5000);
     });
 </script>
-</body>
-</html>`
-	wrappedHTML := fmt.Sprintf(wrappedHTMLBase, constants.RESETCSS, args.HTML)
+<div id="ImgLoadedFlagACHHcLIkD3" style="display:none;">test</div>
+<div id="ACHHcLIkD3">
+`
+
+
+	wrappedHTML := wrappedHTMLBase + args.HTML
 	key := fmt.Sprintf("%d", randInt())
 	lru.SavedHTMLMap.Add(key, wrappedHTML)
 	pageURL := fmt.Sprintf("http://127.0.0.1:%d/html/%s", constants.ServerPort, key)
+	if args.Width > 2000 || args.Width < 10 {
+		args.Width = 750
+	}
 	picbuf, err := shot.Screenshot(pageURL, args.Width)
 	if err != nil {
 		fmt.Println(err)
